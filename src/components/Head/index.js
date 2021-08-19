97e86f2034b1c464ffbe5f823833281a4efe2bdf
haykodarb <haykodarb@gmail.com>
2021-08-19 20:36:44 -0300

    changed app name

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -1,21 +1,21 @@
 import Head from "next/head";
 
 const CustomHead = ({ title }) => {
-  const getTitle = () => {
-    if (title) {
-      return `Hayk - ${title}`;
-    }
+	const getTitle = () => {
+		if (title) {
+			return `Hayk - ${title}`;
+		}
 
-    return "Hayk";
-  };
+		return "Hayk Darbinyan";
+	};
 
-  return (
-    <Head>
-      <title>{getTitle()}</title>
-      <link rel="icon" href="/favicon.ico" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    </Head>
-  );
+	return (
+		<Head>
+			<title>{getTitle()}</title>
+			<link rel="icon" href="/favicon.ico" />
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		</Head>
+	);
 };
 
 export default CustomHead;
