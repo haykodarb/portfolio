57830097790af3f855d7a7a9f29a5a5bfc5a68d5
haykodarb <haykodarb@gmail.com>
2021-09-14 21:55:16 -0300

    Updated portfolio.

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -4,56 +4,62 @@ import Link from "next/link";
 import Container from "../Container";
 
 const Project = ({ data }) => {
-  const { title, internalDetails } = data;
+	const { title, internalDetails } = data;
 
-  const renderDescription = () => {
-    return {
-      __html: internalDetails.description,
-    };
-  };
+	const renderDescription = () => {
+		return {
+			__html: internalDetails.description,
+		};
+	};
 
-  return (
-    <Container>
-      <div className={styles.subContainer}>
-        <Head title={title} />
-        <img src={internalDetails.image} className={styles.image} />
-        <div className={styles.textContainer}>
-          <h3 className={styles.title}>{internalDetails.title}</h3>
-          <p
-            className={styles.description}
-            dangerouslySetInnerHTML={renderDescription()}
-          />
-          <div className={styles.buttonsGroup}>
-            <Link href="/">
-              <p className={styles.textLink}>Inicio</p>
-            </Link>
+	return (
+		<Container>
+			<div className={styles.subContainer}>
+				<Head title={title} />
+				<div className={styles.imageContainer}>
+					{internalDetails.images.map((element) => {
+						console.log(element);
+						return <img src={element} className={styles.image} />;
+					})}
+				</div>
 
-            {internalDetails.githubLink && (
-              <a
-                className={styles.textLink}
-                href={internalDetails.githubLink}
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                GitHub
-              </a>
-            )}
+				<div className={styles.textContainer}>
+					<h3 className={styles.title}>{internalDetails.title}</h3>
+					<p
+						className={styles.description}
+						dangerouslySetInnerHTML={renderDescription()}
+					/>
+					<div className={styles.buttonsGroup}>
+						<Link href="/">
+							<p className={styles.textLink}>Inicio</p>
+						</Link>
 
-            {internalDetails.webLink && (
-              <a
-                className={styles.textLink}
-                href={internalDetails.webLink}
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                Web
-              </a>
-            )}
-          </div>
-        </div>
-      </div>
-    </Container>
-  );
+						{internalDetails.githubLink && (
+							<a
+								className={styles.textLink}
+								href={internalDetails.githubLink}
+								target="_blank"
+								rel="noreferrer noopener"
+							>
+								GitHub
+							</a>
+						)}
+
+						{internalDetails.webLink && (
+							<a
+								className={styles.textLink}
+								href={internalDetails.webLink}
+								target="_blank"
+								rel="noreferrer noopener"
+							>
+								Web
+							</a>
+						)}
+					</div>
+				</div>
+			</div>
+		</Container>
+	);
 };
 
 export default Project;
