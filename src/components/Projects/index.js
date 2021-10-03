16ba26399e1590258eef2acd408140496d32cd4b
haykodarb <haykodarb@gmail.com>
2021-10-03 00:40:44 -0300

    Changed "Technologies" view to be categorized

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -2,18 +2,21 @@ import styles from "./projects.module.css";
 import ProjectLink from "../ProjectLink";
 
 const Projects = ({ data }) => {
-  return (
-    <div className={styles.container}>
-      {data.map((item) => (
-        <ProjectLink
-          key={item.id}
-          href={item.href}
-          title={item.title}
-          description={item.description}
-        />
-      ))}
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<h2 className={styles.title}>Proyectos personales</h2>
+			<div className={styles.subContainer}>
+				{data.map((item) => (
+					<ProjectLink
+						key={item.id}
+						href={item.href}
+						title={item.title}
+						description={item.description}
+					/>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Projects;
--- src/components/Technologies/index.js
+++ src/components/Technologies/index.js
@@ -2,18 +2,38 @@ import styles from "./technologies.module.css";
 
 const Technologies = () => {
 	return (
-		<div className={styles.techContainer}>
-			<p className={styles.techTitle}>Tecnologías utilizadas</p>
-			<ul className={styles.techList}>
-				<li className={styles.listItem}>Node.js</li>
-				<li className={styles.listItem}>React (Web/Native)</li>
-				<li className={styles.listItem}>Next.js</li>
-				<li className={styles.listItem}>Flutter</li>
-				<li className={styles.listItem}>MySQL</li>
-				<li className={styles.listItem}>MongoDB</li>
-				<li className={styles.listItem}>Embedded C / C++</li>
-				<li className={styles.listItem}>Linux</li>
-			</ul>
+		<div className={styles.container}>
+			<h2 className={styles.techTitle}>Tecnologías trabajadas</h2>
+			<div className={styles.subContainer}>
+				<div className={styles.subCategory}>
+					<h3 className={styles.subCategoryTitle}>Front-End</h3>
+					<ul className={styles.techList}>
+						<li className={styles.listItem}>React (Web/Native)</li>
+						<li className={styles.listItem}>Next.js</li>
+						<li className={styles.listItem}>Flutter</li>
+						<li className={styles.listItem}>QT/QML</li>
+					</ul>
+				</div>
+				<div className={styles.subCategory}>
+					<h3 className={styles.subCategoryTitle}>Back-End</h3>
+					<ul className={styles.techList}>
+						<li className={styles.listItem}>Node.js</li>
+						<li className={styles.listItem}>MySQL</li>
+						<li className={styles.listItem}>MongoDB</li>
+						<li className={styles.listItem}>Linux</li>
+					</ul>
+				</div>
+
+				<div className={styles.subCategory}>
+					<h3 className={styles.subCategoryTitle}>Electrónica</h3>
+					<ul className={styles.techList}>
+						<li className={styles.listItem}>Embedded C / C++</li>
+						<li className={styles.listItem}>KiCAD</li>
+						<li className={styles.listItem}>RTOS (ChibiOS)</li>
+						<li className={styles.listItem}>IoT</li>
+					</ul>
+				</div>
+			</div>
 		</div>
 	);
 };
