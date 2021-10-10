3472180fb381624597a293dd9abdd256fc7d0266
haykodarb <haykodarb@gmail.com>
2021-10-09 22:40:43 -0300

    Added images to worked technologies

--- src/components/Technologies/index.js
+++ src/components/Technologies/index.js
@@ -8,29 +8,79 @@ const Technologies = () => {
 				<div className={styles.subCategory}>
 					<h3 className={styles.subCategoryTitle}>Front-End</h3>
 					<ul className={styles.techList}>
-						<li className={styles.listItem}>React (Web/Native)</li>
-						<li className={styles.listItem}>Next.js</li>
-						<li className={styles.listItem}>Flutter</li>
-						<li className={styles.listItem}>QT/QML</li>
+						<li className={styles.listItem}>
+							<img
+								src="/tech/react.png"
+								style={{
+									height: 75,
+									width: 75,
+									alignSelf: "center",
+								}}
+							/>
+							<p>React (Web/Native)</p>
+						</li>
+						<li className={styles.listItem}>
+							<img src="/tech/nextjs.svg" height="75px" />
+							<p>Next.js</p>
+						</li>
+						<li className={styles.listItem}>
+							<img src="/tech/flutter.png" height="75px" />
+							<p>Flutter</p>
+						</li>
+						<li className={styles.listItem}>
+							<img src="/tech/qt.png" height="75px" />
+							<p>QT/QML</p>
+						</li>
 					</ul>
 				</div>
 				<div className={styles.subCategory}>
 					<h3 className={styles.subCategoryTitle}>Back-End</h3>
 					<ul className={styles.techList}>
-						<li className={styles.listItem}>Node.js</li>
-						<li className={styles.listItem}>MySQL</li>
-						<li className={styles.listItem}>MongoDB</li>
-						<li className={styles.listItem}>Linux</li>
+						<li className={styles.listItem}>
+							<img src="/tech/nodejs.png" height="75px" />
+							<p>Node.js</p>
+						</li>
+						<li className={styles.listItem}>
+							<img src="/tech/mysql.png" height="75px" />
+							<p>MySQL </p>
+						</li>
+						<li className={styles.listItem}>
+							<img src="/tech/mongodb.png" height="75px" />
+							<p>MongoDB</p>
+						</li>
+						<li className={styles.listItem}>
+							<img src="/tech/linux.png" height="75px" />
+							<p>Linux</p>
+						</li>
 					</ul>
 				</div>
 
 				<div className={styles.subCategory}>
 					<h3 className={styles.subCategoryTitle}>Electrónica</h3>
 					<ul className={styles.techList}>
-						<li className={styles.listItem}>Embedded C / C++</li>
-						<li className={styles.listItem}>KiCAD</li>
-						<li className={styles.listItem}>RTOS (ChibiOS)</li>
-						<li className={styles.listItem}>IoT</li>
+						<li className={styles.listItem}>
+							<img
+								src="/tech/c.png"
+								style={{
+									height: 75,
+									width: 75,
+									alignSelf: "center",
+								}}
+							/>
+							<p>Embedded C / C++</p>
+						</li>
+						<li className={styles.listItem}>
+							<img src="/tech/kicad.png" height="75px" />
+							<p>KiCAD</p>
+						</li>
+						<li className={styles.listItem}>
+							<img src="/tech/rtos.png" height="75px" />
+							<p>RTOS</p>
+						</li>
+						<li className={styles.listItem}>
+							<img src="/tech/iot.png" height="75px" />
+							<p>IoT</p>
+						</li>
 					</ul>
 				</div>
 			</div>
