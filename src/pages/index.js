3f760b9818dc83b61922be40d6575b921ac33165
haykodarb <haykodarb@gmail.com>
2021-10-10 20:55:27 -0300

    Added about me page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,19 +3,21 @@ import Container from "../components/Container";
 import Technologies from "../components/Technologies";
 import Projects from "../components/Projects";
 import Hero from "../components/Hero";
+import AboutMe from "../components/AboutMe";
 import { getProjects } from "../helpers/getProjects";
 
 export default function Index() {
-  const projectsData = getProjects();
+	const projectsData = getProjects();
 
-  return (
-    <>
-      <Head />
-      <Container>
-        <Hero />
-        <Projects data={projectsData.data} />
-        <Technologies />
-      </Container>
-    </>
-  );
+	return (
+		<>
+			<Head />
+			<Container>
+				<Hero />
+				<Projects data={projectsData.data} />
+				<Technologies />
+				<AboutMe />
+			</Container>
+		</>
+	);
 }
