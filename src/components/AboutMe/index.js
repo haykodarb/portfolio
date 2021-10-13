110aa020a6599008a94916e1544aab32e0a24570
haykodarb <haykodarb@gmail.com>
2021-10-13 18:03:04 -0300

    Changed about me page

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -5,13 +5,16 @@ const AboutMe = () => {
 		<div className={styles.container}>
 			<h2 className={styles.title}>About me</h2>
 			<p className={styles.text}>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-				veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-				commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-				velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-				occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-				mollit anim id est laborum.
+				Mi nombre es Hayk Darbinyan, soy un estudiante de Ingeniería Mecánica en
+				la ciudad de San Carlos de Bariloche en la provincia de Rio Negro. Me
+				dedico profesionalmente al desarrollo de hardware y software, tanto en
+				mi trabajo como en mi tiempo personal. Esto incluye la creación
+				dispositivos electrónicos, tanto en el diseño del circuito y el PCB como
+				en el desarrollo del firmware, así como también la creación de
+				servidores y aplicaciones web y moviles.
+				<br />
+				Además de todo esto soy también un entusiasta del Free Software y Open
+				Hardware.
 			</p>
 		</div>
 	);
