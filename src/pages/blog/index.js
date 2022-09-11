6ba793576fc13b0ea134d2effbb35b94f9f5afe3
haykodarb <work@hayk.ar>
2022-09-11 20:35:45 -0300

    Fixed font in header title

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -16,21 +16,23 @@ function Header() {
 
 
 export default function BlogIndex({ posts }) {
-    console.log(posts);
-    return <div className={styles['container']}>
-        <Head title='Blog'/>
-        <Header />
-        <h1 className={styles['title']}>Blog</h1>
-        {posts.map(element => {
-            return <div key={element.file}>
-                <Link href={`/blog/${element.file}`}>
-                    <h4 className={styles['subtitle']}>{element.data.title}</h4>
-                </Link>
 
-                <p>{element.data.description}</p>
-            </div>
-        })}
-    </div>;
+    return <>
+        <Head title='Blog' />
+        <Header />
+        <div className={styles['container']}>
+            <h1 className={styles['title']}>Blog</h1>
+            {posts.map(element => {
+                return <div key={element.file}>
+                    <Link href={`/blog/${element.file}`}>
+                        <h4 className={styles['subtitle']}>{element.data.title}</h4>
+                    </Link>
+
+                    <p>{element.data.description}</p>
+                </div>
+            })}
+        </div>
+    </>;
 }
 
 export async function getStaticProps({ params }) {
