02ed15a1078252cf49e6e9f060637b4f50319d61
haykodarb <work@hayk.ar>
2023-06-11 10:18:10 -0300

    Added communal landing page

---
+++ src/pages/communal/index.js
@@ -0,0 +1,11 @@
+
+export default function BlogIndex({ posts }) {
+
+    return <>
+        {/* <Head title='Blog' /> */}
+        {/* <Header /> */}
+        <div >
+            <h3 style={{alignItems: 'center', justifyContent: 'center', width: '100%', display: 'flex', textAlign:'center'}}>You have successfully activated your account.<br/>You may now login.</h3>
+        </div>
+    </>;
+}
