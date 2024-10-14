03b1584541618112c53f5ba7ec9c29ccc4e1dc06
Hayk Darbinyan <work@hayk.ar>
2024-10-14 16:08:17 -0300

    Added arev description and highlighting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,15 @@
-<head>
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-	<link
-		href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-		rel="stylesheet"
-	/>
-</head>
+<script>
+	import '@fontsource/poppins/400.css';
+	import '@fontsource/poppins/500.css';
+	import '@fontsource/poppins/600.css';
+	import '@fontsource/poppins/700.css';
+	import '@fontsource/poppins/800.css';
+	import '@fontsource/poppins/400-italic.css';
+	import '@fontsource/poppins/500-italic.css';
+	import '@fontsource/poppins/600-italic.css';
+	import '@fontsource/poppins/700-italic.css';
+	import '@fontsource/poppins/800-italic.css';
+</script>
 
 <div id="page">
 	<div id="body">
@@ -31,28 +35,39 @@
 		<div class="category-container">
 			<h1 class="category-title">Skills</h1>
 			<ul>
-				<li>Profficient in C, C++, Dart and Javascript.</li>
-				<li>Knowledgeable about RTOS, have used ChibiOS and FreeRTOS extensively.</li>
 				<li>
-					Ample work with STM32 microcontrollers, including configuring from scratch, interfacing
-					with clocks, hardware timers, interrupts, SPI/I2C/USB/CAN and more.
+					Profficient in
+					<span class="highlighted">C, C++, Dart and Javascript</span>
+					.
 				</li>
-				<li>Experience in building IoT devices using ESP32 microcontrollers.</li>
 				<li>
-					Mobile, desktop and web app development with Flutter, Qt and various Javascript
-					frameworks.
+					Knowledgeable about
+					<span class="highlighted">Real Time Operating Systems</span>, have used ChibiOS and
+					FreeRTOS extensively.
 				</li>
-				<li>Backend development and PostgreSQL/MySQL database management.</li>
 				<li>
-					Competent with Linux, built several servers with it and have been using it as my main
-					operating systems for 5 years now. Currently running Ubuntu for my personal server and
-					NixOS for my desktop machine.
+					Ample work with <span class="highlighted">STM32 microcontrollers</span>, including
+					configuring from scratch, interfacing with clocks, hardware timers, interrupts,
+					SPI/I2C/USB/CAN and more.
 				</li>
+				<li>
+					Experience in building <span class="highlighted">IoT devices</span> using WiFi and Bluetooth.
+				</li>
+				<li>
+					Mobile, desktop and web app development with <span class="highlighted"
+						>Flutter, Qt and various Javascript frameworks</span
+					>.
+				</li>
+				<li>Database management using <span class="highlighted">PostgreSQL/MySQL</span>.</li>
+				<li>
+					<span class="highlighted">Linux</span> for use in both desktops and servers.
+				</li>
+				<li>Schematic and PCB design and review</li>
 				<li>
 					Worked developing tools and products for diverse industries such as space, chemistry,
 					health, agriculture and transport.
 				</li>
-				<li>Fluent in Spanish, English and Armenian.</li>
+				<li>Fluent in English, Spanish and Armenian.</li>
 			</ul>
 		</div>
 
@@ -62,18 +77,23 @@
 			<ul>
 				<div>
 					<li class="list-title">
-						<a target=”_blank” href="https://aratom.com/" class="subtitle link">Aratom (full time)</a>
-						<p class="list-title date">2023 - ongoing</p>
+						<a target="”_blank”" href="https://aratom.com/" class="subtitle link">Aratom</a>
+						<p class="list-title date">2020 - ongoing</p>
 					</li>
 					<ul>
 						<li>Firmware and hardware development in space related project.</li>
 						<li>Management of bussiness related servers.</li>
 						<li>General IT responsibilities.</li>
+						<li>
+							Firmware and hardware development in electronic seeder project. (joint project with
+							Emtech)
+						</li>
+						<li>Firmware, hardware and mobile app development for truck weight-scale project.</li>
 					</ul>
 				</div>
 				<div style="margin-top: 1em;">
 					<li class="list-title">
-						<a target=”_blank” href="https://www.emtechsa.com/" class="subtitle link">Emtech (part time)</a>
+						<a target="”_blank”" href="https://www.emtechsa.com/" class="subtitle link">Emtech</a>
 						<p class="list-title date">2020 - 2023</p>
 					</li>
 					<ul>
@@ -82,35 +102,24 @@
 						<li>Embedded Linux app development for chemical engineering company.</li>
 					</ul>
 				</div>
-				<div style="margin-top: 1em;">
-					<li class="list-title">
-						<a target=”_blank” href="https://aratom.com/" class="subtitle link">Aratom (part time)</a>
-						<p class="list-title date">2020 - 2023</p>
-					</li>
-					<ul>
-						<li>
-							Firmware and hardware development in electronic seeder project. (joint project with
-							Emtech)
-						</li>
-						<li>Firmware, hardware and mobile app development for truck weight-scale project.</li>
-					</ul>
-				</div>
 			</ul>
 		</div>
 		<div class="category-container">
 			<h1 class="category-title">Personal Projects</h1>
 			<ul>
 				<li style="list-style-type: none; padding: 0; margin: 0;">
-					<a target=”_blank” href="https://communal.ar" class="subtitle link">Communal</a>
-					<p style="margin: 0; padding: 0;">Semi-decentralized physical book sharing app.</p>
+					<a target="”_blank”" href="https://communal.ar" class="subtitle link">Communal</a>
+					<p style="margin: 0; padding: 0;">Semi-decentralized physical book sharing platform.</p>
 				</li>
 				<li style="list-style-type: none; padding: 0; margin: 0; margin-top: 1em;">
-					<a target=”_blank” href="https://kassen.hayk.ar" class="subtitle link">Kassen</a>
+					<a target="”_blank”" href="https://kassen.hayk.ar" class="subtitle link">Kassen</a>
 					<p style="margin: 0; padding: 0;">Greenhouse temperature monitoring station.</p>
 				</li>
 				<li style="list-style-type: none; padding: 0; margin: 0; margin-top: 1em;">
-					<a target=”_blank” href="/arev" class="subtitle link">Arev</a>
-					<p style="margin: 0; padding: 0;">Waterheater temperature controller.</p>
+					<a href="/arev" class="subtitle link">Arev</a>
+					<p style="margin: 0; padding: 0;">
+						Solar waterheater temperature monitor and controller.
+					</p>
 				</li>
 			</ul>
 		</div>
@@ -212,7 +221,7 @@
 	}
 
 	.subtitle.link {
-		font-weight: 600;
+		font-weight: 700;
 		color: var(--primary);
 		text-decoration: none;
 	}
@@ -224,5 +233,10 @@
 
 	.category-container {
 		margin-top: 2.5vh;
+	}
+
+	.highlighted {
+		font-weight: 600;
+		color: #8839ef;
 	}
 </style>
---
+++ src/routes/arev/+page.svelte
@@ -0,0 +1,52 @@
+<script>
+	import '@fontsource/poppins/400.css';
+	import '@fontsource/poppins/500.css';
+	import '@fontsource/poppins/600.css';
+	import '@fontsource/poppins/700.css';
+	import '@fontsource/poppins/800.css';
+	import '@fontsource/poppins/400-italic.css';
+	import '@fontsource/poppins/500-italic.css';
+	import '@fontsource/poppins/600-italic.css';
+	import '@fontsource/poppins/700-italic.css';
+	import '@fontsource/poppins/800-italic.css';
+</script>
+
+<div id="page">
+	<div id="body">
+		<h1>Arev</h1>
+		<p>
+			The project consists of a custom built solar water heater controller built around an ESP8266
+			microcontroller.
+			<br />
+			<br />
+			The device is controlled via an Android app that I built using Flutter. On startup, it connects
+			to the WiFi networks and opens up a local webserver and is discoverable over mDNS, the user then
+			uses the app to connect to said webserver and either read the current temperatures values, obtain
+			previous temperature and power consumption logs or modify the setpoints which the controller uses
+			to turn the electric resistance on or off.
+		</p>
+	</div>
+</div>
+
+<style>
+	#page {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: start;
+		font-family: 'Poppins';
+		padding: 0;
+		margin: 0;
+		color: var(--text-color);
+		width: 100%;
+	}
+
+	#body {
+		width: 90vw;
+		max-width: 700px;
+		display: flex;
+		flex-direction: column;
+		padding-top: 5vh;
+		padding-bottom: 5vh;
+	}
+</style>
